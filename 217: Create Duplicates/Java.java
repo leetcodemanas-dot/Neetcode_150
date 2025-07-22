12a6d60127093dc73585ca7a6ffd567c1cb4de9a
leetcodemanas-dot <leetcode.manas@gmail.com>
2025-07-22 15:20:54 +0530

    Update Java.java

--- 217: Create Duplicates/Java.java
+++ 217: Create Duplicates/Java.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      HashSet h2 = new HashSet<>();4
+      HashSet h2 = new HashSet<>();
       for(int num : nums) {
         if(h2.contains(num)) {
             return true;
